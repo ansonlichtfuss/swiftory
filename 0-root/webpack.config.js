598febf3ee2b70af99bc6b0058150a3b45771fc5
Anson <git@lichtf.us>
2021-02-27 14:52:29 -0700

    chore: add robots.txt

--- 0-root/webpack.config.js
+++ 0-root/webpack.config.js
@@ -1,12 +1,13 @@
-const { merge } = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa-ts");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require('webpack-merge');
+const singleSpaDefaults = require('webpack-config-single-spa-ts');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "swiftory";
+  const orgName = 'swiftory';
   const defaultConfig = singleSpaDefaults({
     orgName,
-    projectName: "root-config",
+    projectName: 'root-config',
     webpackConfigEnv,
     argv,
     disableHtmlGeneration: true,
@@ -17,11 +18,14 @@ module.exports = (webpackConfigEnv, argv) => {
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
-        template: "src/index.ejs",
+        template: 'src/index.ejs',
         templateParameters: {
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
           orgName,
         },
+      }),
+      new CopyPlugin({
+        patterns: [{ from: 'src/robots.txt', to: 'robots.txt' }],
       }),
     ],
   });
