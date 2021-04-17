41183999334fdfbc4a1e3bbf5a18341335ca3220
Anson <git@lichtf.us>
2021-04-17 11:10:21 -0600

    feat: mvp layout for jukebox

--- 2-jukebox/src/Dashboard.svelte
+++ 2-jukebox/src/Dashboard.svelte
@@ -4,39 +4,57 @@ import albumData from "./album-data";
 import Jukebox from "./svg/Jukebox.svelte";
 
 export let allFoundKeys;
-let emptyAlbumArray = new Array(21).fill("");
+let emptyAlbumArray = new Array(Object.keys(albumData).length).fill("");
 </script>
 
 <div>
-  <section class="py-24 px-4">
-    <div class="flex justify-center w-full max-w-4xl mx-auto">
-      <div class="relative" style="width:200px;z-index:1;">
+  <section class="py-24 px-4 w-full">
+    <div
+      class="flex flex-col items-center justify-center w-full max-w-xl mx-auto">
+      <div class="relative" style="width:200px;z-index:1;margin-bottom:-85px;">
         <Jukebox />
       </div>
       <div
-        class="flex-grow border p-5 rounded"
-        style="margin-left:-100px;padding-left:110px;border-color:#555;">
+        class="flex-grow border-4 p-5 rounded-lg w-full"
+        style="border-color:#493b33;">
         <div
-          class="flex justify-between border-b pb-3 mb-3 text-center"
-          style="border-bottom-color:#555;">
+          class="flex justify-between border-b pb-3 mb-6 text-center"
+          style="border-bottom-color:#493b33;">
           <span class="font-black">JUKEBOX</span>
           <span><strong>{allFoundKeys.length}</strong>
             of
             <strong>{emptyAlbumArray.length}</strong>
             albums found</span>
         </div>
-        {#each emptyAlbumArray as albumPlaceholder, index}
-          <div
-            class="border border-white rounded-sm"
-            style="width:50px;height:50px;">
-            {#if allFoundKeys[index]}
-              <img
-                src="{`${CDN_URL}/jukebox/albums/${albumData[allFoundKeys[index]].cover}`}" />
-            {/if}
-            poop album
-          </div>
-        {/each}
-        {JSON.stringify(allFoundKeys)}
+        <div class="grid grid-cols-2">
+          {#each emptyAlbumArray as albumPlaceholder, index}
+            <div class="flex items-center my-2">
+              <div
+                class="rounded-sm overflow-hidden"
+                style="width:50px;height:50px;background:#261e1a;">
+                {#if allFoundKeys[index]}
+                  <img
+                    src="{`${CDN_URL}/jukebox/albums/${albumData[allFoundKeys[index]].cover}`}"
+                    alt="{albumData[allFoundKeys[index]].album}" />
+                {/if}
+              </div>
+              <div class="flex flex-col flex-grow justify-center ml-4">
+                {#if allFoundKeys[index]}
+                  <strong>{albumData[allFoundKeys[index]].album}</strong>
+                  <small>by
+                    <em>{albumData[allFoundKeys[index]].artist}</em></small>
+                {:else}
+                  <div
+                    class="rounded mb-2"
+                    style="width:150px;height:1rem;background:#261e1a;"></div>
+                  <div
+                    class="rounded"
+                    style="width:50px;height:1rem;background:#261e1a;"></div>
+                {/if}
+              </div>
+            </div>
+          {/each}
+        </div>
       </div>
     </div>
   </section>
