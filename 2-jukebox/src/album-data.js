03d15ecf61e5aa605d85320e0fad9676aaaef2f8
Anson <git@lichtf.us>
2021-04-17 09:56:58 -0600

    feat: add album data for Folklore and Reputation

--- 2-jukebox/src/album-data.js
+++ 2-jukebox/src/album-data.js
@@ -1,4 +1,27 @@
 export default {
+  // Folklore Album
+  aaron_dessner_sleep_well_beast: {
+    producer: "Aaron Dessner",
+    artist: "The National",
+    album: "Sleep Well Beast",
+    year: 2017,
+    cover: "the-national-sleep-well-beast.jpg",
+  },
+  jack_antonoff_some_nights: {
+    producer: "Jack Antonoff",
+    artist: "Fun",
+    album: "Some Nights",
+    year: 2012,
+    cover: "fun-some-nights.jpg",
+  },
+  justin_vernon_22_a_million: {
+    producer: "Justin Vernon",
+    artist: "Bon Iver",
+    album: "22, A Million",
+    year: 2016,
+    cover: "bon-iver-22-a-million.jpg",
+  },
+  // Lover Album
   jack_antonoff_melodrama: {
     producer: "Jack Antonoff",
     artist: "Lorde",
@@ -26,5 +49,27 @@ export default {
     album: "Busyhead",
     year: 2019,
     cover: "noah-kahan-busyhead.jpg",
+  },
+  // Reputation Album
+  jack_antonoff_chemtrails_over_the_country_club: {
+    producer: "Jack Antonoff",
+    artist: "Lana Del Rey",
+    album: "Chemtrails over the Country Club",
+    year: 2021,
+    cover: "lana-del-rey-chemtrails-over-the-country-club.jpg",
+  },
+  shellback_25: {
+    producer: "Shellback",
+    artist: "Adele",
+    album: "25",
+    year: 2015,
+    cover: "adele-25.jpg",
+  },
+  max_martin_after_hours: {
+    producer: "Max Martin",
+    artist: "The Weeknd",
+    album: "After Hours",
+    year: 2020,
+    cover: "the-weeknd-after-hours.jpg",
   },
 };
